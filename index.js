596da161d9d153ddd4aa45f8765dd15606209b0c
ahrke <david.duy.minh@gmail.com>
2019-07-24 23:20:40 +0000

    set up sum() to work recursively

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  // base-case: when our fromN === toN
+  // recursive-case: when fromN < toN
+  //
+
+  if (fromN === toN) return toN;
+
+  return fromN + sum((fromN + 1), toN)
 }
 
 module.exports = sum;
